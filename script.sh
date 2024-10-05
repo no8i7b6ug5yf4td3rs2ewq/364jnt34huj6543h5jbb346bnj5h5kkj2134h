ab2eee11a44bcda27a9939fdfe6bd42e669428a9
no8i7b6ug5yf4td3rs2ewq <122082822+no8i7b6ug5yf4td3rs2ewq@users.noreply.github.com>
2024-10-05 19:42:53 +0000

    --

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ for i in $(seq 1 6);
 do
     echo "chapter $i"
     echo "<tr><td>$i</td>" >> $FILE
-    lastM=`curl -Is $URL/capitolo$i.pdf | grep "Last-Modified"`
+    lastM=`curl -Is $URL/capitolo$i.pdf | grep "Last-Modified" | cut -c16-`
     echo "<td>$lastM</td>" >> $FILE
     echo "</tr>" >> $FILE
 done
