bf005830247d930dab11ff4f32609a5ffd22791d
no8i7b6ug5yf4td3rs2ewq <122082822+no8i7b6ug5yf4td3rs2ewq@users.noreply.github.com>
2024-10-05 19:37:33 +0000

    --

--- script.sh
+++ script.sh
@@ -1,7 +1,16 @@
+FILE="index.html"
+echo "<h1>AA Chapters</h1>" > $FILE
+echo "<table><tr><th>Chapter</th><th>Last Modified</th></tr>" >> $FILE
+
 for i in $(seq 1 6);
 do
-    echo "capitolo$i"
-    wget -O capitolo$i.pdf http://intranet.di.unisa.it/~robdep/AA/dispensa/capitolo$i.pdf
+    echo "chapter $i"
+    echo "<tr><td>$i</td>" >> $FILE
+    lastM=`curl -Is $URL/capitolo$i.pdf | grep "Last-Modified"`
+    echo "<td>$lastM</td>" >> $FILE
+    echo "</tr>" >> $FILE
 done
+
+echo "</table>" >> $FILE
 
 echo "done"
