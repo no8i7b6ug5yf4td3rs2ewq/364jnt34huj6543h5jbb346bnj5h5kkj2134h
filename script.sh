ce930d26388957925d75f529d61a826a6dab152f
no8i7b6ug5yf4td3rs2ewq <122082822+no8i7b6ug5yf4td3rs2ewq@users.noreply.github.com>
2024-10-13 21:35:49 +0200

    added etag

--- script.sh
+++ script.sh
@@ -1,13 +1,16 @@
 FILE="index.html"
 echo "<h1>AA Chapters</h1>" > $FILE
-echo "<table><tr><th>Chapter</th><th>Last Modified</th></tr>" >> $FILE
+echo "<table><tr><th>Chapter</th><th>Last Modified</th><th>ETag</th></tr>" >> $FILE
 
 for i in $(seq 1 6);
 do
     echo "chapter $i"
     echo "<tr><td>$i</td>" >> $FILE
-    lastM=`curl -Is $URL/capitolo$i.pdf | grep "Last-Modified" | cut -c16-`
+    data=`curl -Is $URL/capitolo$i.pdf`
+    lastM=`echo $data | grep "Last-Modified" | cut -c16-`
+    etag=`echo $data | grep "ETag" | cut -c6-`
     echo "<td>$lastM</td>" >> $FILE
+    echo "<td>$etag</td>" >> $FILE
     echo "</tr>" >> $FILE
 done
 
